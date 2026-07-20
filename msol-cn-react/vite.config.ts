15e6139a57eb7481455bad7de4f2e3a32e819cfe
Mrwr <2268922921@qq.com>
2026-07-20 15:01:28 +0800

    add github pages deployment

--- msol-cn-react/vite.config.ts
+++ msol-cn-react/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/msol-cn/',  // 改成你的 GitHub 仓库名
   server: {
     host: true,
     allowedHosts: true,
